aa7df431eeeb96afdd45299d37121917787817a3
geoiordache <george.iordache20@gmail.com>
2014-12-05 14:01:06 +0200

    Created project running on two platforms : desktop and html
    The entry point to the application is SpaceInvaders.java located in core
    directory.

---
+++ html/src/mta/se/project/spaceinvaders/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package mta.se.project.spaceinvaders.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import mta.se.project.spaceinvaders.SpaceInvaders;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SpaceInvaders();
+        }
+}
--- src/Main.java
+++
@@ -1,5 +0,0 @@
-/**The entry point to the application
- * Created by George on 11/30/2014.
- */
-public class Main {
-}
