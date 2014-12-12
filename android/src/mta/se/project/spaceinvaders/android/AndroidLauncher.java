aa0a5c182daaa50f916ffced1f6a667c471c51f8
geoiordache <george.iordache20@gmail.com>
2014-12-12 18:51:43 +0200

    Resolving some errors

---
+++ android/src/mta/se/project/spaceinvaders/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package mta.se.project.spaceinvaders.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import mta.se.project.spaceinvaders.SpaceInvaders;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceInvaders(), config);
+	}
+}
