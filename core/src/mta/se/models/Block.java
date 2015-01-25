c19f5c2045196a78c2b74a85ace944808c2e6ee4
geoiordache <george.iordache20@gmail.com>
2015-01-25 14:24:59 +0200

    added more models

---
+++ core/src/mta/se/models/Block.java
@@ -0,0 +1,13 @@
+package mta.se.models;
+
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Vector3;
+
+public class Block extends ModelInstance {
+	public final static float BLOCK_RADIUS = 0.5f;
+
+	public Block (Model model, float x, float y, float z) {
+		super(model, x, y, z);
+	}
+}
